e8d3d012471e2250f289153e1e582c7b924f2b4e
mohamed <mohamedsayedahmd@gmail.com>
2023-08-23 20:21:16 +0300

    set the properties and create RsaProperties class

--- README.md
+++ README.md
@@ -6,3 +6,17 @@
 - OAuth2 Resource Server
 - Spring Configuration Processor
 ___
+gen keypair
+```openssl genrsa -out keypair.pem 2048```
+\
+\
+create public key
+```openssl rsa -in keypair.pem -pubout -out public.pem```
+\
+\
+create private key
+```openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out private.pem```
+\
+\
+now we can delete keypar.pem
+___
--- src/main/java/com/auth/jwtdemo/config/SecurityConfig.java
+++ src/main/java/com/auth/jwtdemo/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -38,6 +39,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(auth -> auth
                         .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic()
                 .and()
