aface7f16d4aee523ace7f068968749b0aa62fe6
mohamed <mohamedsayedahmd@gmail.com>
2023-08-23 19:53:15 +0300

    edit SecurityConfig

--- src/main/java/com/auth/jwtdemo/config/SecurityConfig.java
+++ src/main/java/com/auth/jwtdemo/config/SecurityConfig.java
@@ -2,27 +2,48 @@ package com.auth.jwtdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager user() {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+
+        return new InMemoryUserDetailsManager(
+                User.withUsername("mohamed")
+                        .password(passwordEncoder.encode("mohamed123")) // Encode the password
+                        .authorities("read")
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Use BCryptPasswordEncoder for password encoding
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(auth -> auth
                         .anyRequest().authenticated())
-                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(withDefaults())
-        .build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic()
+                .and()
+                .build();
     }
 }
+
 // never disable csrf protection while leaving session management enabled
 // doing so will open you up to a csrf attack
--- src/main/java/com/auth/jwtdemo/controller/HomeController.java
+++ src/main/java/com/auth/jwtdemo/controller/HomeController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping("/")
+//@RequestMapping("/")
 public class HomeController {
     @GetMapping
-    public String home(){
-        return "Hello, JWT!";
+    public String home(Principal principal){
+        return "Hello, JWT! "+ principal.getName();
     }
 
 }
